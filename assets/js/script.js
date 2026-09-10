c3b852b0a50c92adc56f21b415bb4747958c9e62
n-ham <nicholas.charles.ham@gmail.com>
2026-09-10 10:37:33 +1000

    Highlight @pathtofile and @pathtopage in the Nift syntax grammar
    The website highlighter recognized @path and @pathto but not the other two
    supported path-family directives. Both call forms are now highlighted; the
    @path/@pathto/@pathtofile/@pathtopage lookaheads disambiguate without
    touching prose occurrences.

--- assets/js/script.js
+++ assets/js/script.js
@@ -270,7 +270,7 @@
       hljs.registerLanguage('nift', () => ({
         name: 'Nift',
         contains: [
-          { className: 'meta', begin: /\\?@(?:content\b|input(?=\()|path(?=\()|pathto(?=\()|getenv(?=\()|ent(?=\()|dep(?=\()|json(?=\()|for(?=\()|if(?=\())/ },
+          { className: 'meta', begin: /\\?@(?:content\b|input(?=\()|path(?=\()|pathto(?=\()|pathtofile(?=\()|pathtopage(?=\()|getenv(?=\()|ent(?=\()|dep(?=\()|json(?=\()|for(?=\()|if(?=\())/ },
           { className: 'meta', begin: /\\?\$\[[^\]\n]+\]/ },
           { className: 'string', begin: /'/, end: /'/ },
           { className: 'string', begin: /"/, end: /"/ },
