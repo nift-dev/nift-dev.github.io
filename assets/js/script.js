03985741054d7c23b26b12d6edb779a2ff2e2f89
n-ham <nicholas.charles.ham@gmail.com>
2026-09-20 02:16:48 +1000

    Highlight ; as a shell operator in the Nift grammar

--- assets/js/script.js
+++ assets/js/script.js
@@ -273,7 +273,7 @@
           { className: 'meta', begin: /\\?@(?:content\b|input(?=\()|inject(?=\()|path(?=\()|pathto(?=\()|pathtofile(?=\()|pathtopage(?=\()|getenv(?=\()|ent(?=\()|dep(?=\()|script\b|import(?=\()|for(?=\()|while(?=\()|if(?=\()|fn(?=\()|fragment(?=\()|struct(?=\()|:=(?=\())/ },
           { className: 'meta', begin: /\\?\$\[/, end: /(?=.)/, excludeEnd: true },
           { className: 'keyword', begin: /\b(?:if|else|for|while|return|break|continue|fn|private|const|immut|export|null|run|cmd)\b/ },
-          { className: 'operator', begin: /\.\.\.|2>&1|2>|>>|:=|==|!=|<=|>=|&&|\|\||[|&+*\/%?:<>!=\-]/ },
+          { className: 'operator', begin: /\.\.\.|2>&1|2>|>>|:=|==|!=|<=|>=|&&|\|\||[|&+*\/%?:<>!=\-;]/ },
           { className: 'number', begin: /\b(?:\d+(?:\.\d+)?)\b/ },
           { className: 'string', begin: /'/, end: /'/ },
           { className: 'string', begin: /"/, end: /"/ },
